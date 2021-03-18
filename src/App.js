ea26983e1961d7b79cc1c9ce32d2529fd1792bed
Nika <delmare@mail.ru>
2021-03-18 15:19:37 +0300

    hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+
+function computeInitialCounter() {
+  console.log('some calc')
+  return Math.trunc(Math.random() * 20)
+}
 
 function App() {
+  //const [counter, setCounter]= useState(0)
+  //const [counter, setCounter]= useState(computeInitialCounter())
+  const [counter, setCounter]= useState(() => {
+    return computeInitialCounter()
+  })
+
+  const [state, setState] = useState({
+    title: 'counter',
+    date: Date.now()
+  })
+
+  function increment() {
+    //setCounter(counter + 1)
+    setCounter((prevCounter) => {
+      return prevCounter + 1
+    })
+    setCounter(prev => prev + 1)
+  }
+
+  function decrement() {
+    setCounter(counter - 1)
+  }
+
+  function updateTitle() {
+    setState(prev => {
+      return {
+        ...prev,
+        title: 'New title' 
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={increment} className='btn btn-success'>Add</button>
+      <button onClick={decrement} className='btn btn-danger'>Delete</button>
+      <button onClick={updateTitle} className='btn btn-default'>Update title</button>
+
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   );
 }
