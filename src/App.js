8f036bd240078622df4989361a1221a9d3cb44cc
Nika <delmare@mail.ru>
2021-03-19 11:56:24 +0300

    useContext

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React, {useCallback, useState} from 'react'
-
-import ItemsList from './ItemsList'
+import Alert from './Alert/Alert'
+import {AlertProvider} from './Alert/AlertContext'
+import Main from './Main'
+import React from 'react'
 
 function App() {
-  const [colored, setColored] = useState(false)
-
-  const [count, setCount] = useState(1)
-
-  const styles = {
-    color: colored ? 'darkred' : 'black'    
-  }
-
-  const generateItemsFromAPI = useCallback(() => {
-    return new Array(count).fill('').map((_, i) => `Element ${i + 1}`)
-  }, [count])
-
   return (
-    <>
-      <h1 style={styles}>Calculate: {count}</h1>
-      <button className = 'btn btn-success' onClick={() => setCount(prev => prev + 1)}>Add</button>    
-      <button className = 'btn btn-warning' onClick={() => setColored(prev => !prev)}>Change</button>
-      <ItemsList getItems={generateItemsFromAPI}/>  
-    </>
+    <AlertProvider>
+      <div className={'container pt-3'}>
+        <Alert />
+        <Main toggle={() => {}}/>
+      </div>
+    </AlertProvider>
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useAlert } from './Alert/AlertContext'
+export default function Main() {
+    const {toggle} = useAlert()
+    return (
+        <>
+            <h1>Context</h1>
+            <button onClick={toggle} className='btn btn-success'>Alert</button>            
+        </>
+    )
+}
