460a7ad60feba366a3b0d557f7703f71ccdcb4e4
Nika <delmare@mail.ru>
2021-03-19 14:21:48 +0300

    Myhooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import Alert from './Alert/Alert'
-import {AlertProvider} from './Alert/AlertContext'
-import Main from './Main'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+function useLogger(value) {
+  useEffect(() => {
+    console.log('value changed: ', value)
+  }, [value])
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = event => {
+    setValue(event.target.value)
+  }
+
+  const clear = () => setValue('')
+
+  return {
+    bind: {value, onChange},
+    value,
+    clear
+  }
+}
 
 function App() {
-  return (
-    <AlertProvider>
-      <div className={'container pt-3'}>
-        <Alert />
-        <Main toggle={() => {}}/>
-      </div>
-    </AlertProvider>
+  const input = useInput('')
+  useLogger(input.value)
+  return (    
+    <div className={'container pt-3'}>      
+      <input type="text" {...input.bind} />
+
+      <button className="btn btn-warning" onClick={() => input.clear()}>Clear</button>
+      <hr />
+      <h1>{input.value}</h1>
+    </div>
   );
 }
 
